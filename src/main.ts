ac36044f03394eef7e7388edfad2380526a042cd
nombre Prueba <alexhernandez.hr15@gmail.com>
2025-06-20 12:22:19 +0200

    cena amigos

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const grupoamigos = 6;
+const invitobebidas = 18;
+const ticketcena = 120;
+const precioporamigo = ticketcena - invitobebidas;
+console.log(precioporamigo / grupoamigos);
